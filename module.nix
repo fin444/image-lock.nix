eaca180a24c429337e79a1633c00133107e0d50d
fin444 <fin444@users.noreply.github.com>
2024-09-02 17:55:03 -0400

    refactor

--- module.nix
+++ module.nix
@@ -10,21 +10,21 @@
 		};
 	};
 
-	config = {
+	config = let
+		data = (builtins.fromJSON (builtins.readFile cfg.lockFile));
+		cfg = config.environment.image-lock;
+	in {
 		# if there are no containers defined, it'll throw an error without this
 		environment.image-lock.containers = {};
 
-		virtualisation.oci-containers.containers =
-			let data = (builtins.fromJSON (builtins.readFile config.environment.image-lock.lockFile));
-			in builtins.mapAttrs
-				(name: value:
-					if (lib.attrsets.hasAttr "digest" data."${value}") then {
-						image = "${value}@${data."${value}".digest}";
-					} else {
-						image = data."${value}".finalImageName;
-						imageFile = pkgs.dockerTools.pullImage data."${value}";
-					}
-				)
-				config.environment.image-lock.containers;
+		# define images for all containers
+		virtualisation.oci-containers.containers = builtins.mapAttrs (name: value:
+			if (lib.attrsets.hasAttr "digest" data."${value}") then {
+				image = "${value}@${data."${value}".digest}";
+			} else {
+				image = data."${value}".finalImageName;
+				imageFile = pkgs.dockerTools.pullImage data."${value}";
+			}
+		) cfg.containers;
 	};
 }
